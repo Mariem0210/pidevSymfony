11249c9ade4d173a7bfb8d4449b5d50fbfd75ca8
KADRSO2810 <mohamedaziz.ayachi@esprit.tn>
2025-04-07 18:37:28 +0100

    controle saisie transfert

--- src/Form/TransfertType.php
+++ src/Form/TransfertType.php
@@ -6,6 +6,8 @@ use App\Entity\Transfert;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class TransfertType extends AbstractType
 {
@@ -15,7 +17,18 @@ class TransfertType extends AbstractType
             ->add('idu')
             ->add('ancienne_equipe')
             ->add('nouvelle_equipe')
-            ->add('montantt')
+            ->add('montantt', NumberType::class, [
+                'constraints' => [
+                    new Positive([
+                        'message' => 'Le montant doit être positif'
+                    ])
+                ],
+                'attr' => [
+                    'min' => 0.01,
+                    'step' => '0.01',
+                    'required' => true
+                ]
+            ])
             ->add('datet', null, [
                 'widget' => 'single_text',
             ])
